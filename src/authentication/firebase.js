faee3e2bd8c642eb332bf00d85eb9d30acd9e544
Ayoub Bougroune <68024921+ayoub-bougroune@users.noreply.github.com>
2022-05-22 15:47:16 +0100

    Update firebase.js

--- src/authentication/firebase.js
+++ src/authentication/firebase.js
@@ -4,9 +4,9 @@ import "firebase/auth"
 const app = firebase.initializeApp({
   apiKey: "AIzaSyAP_Ran0zickSzadGnJXTmkGRScZuoNcOU",
   authDomain: "parkingfinder-6321f.firebaseapp.com",
-  databaseURL: "https://parkingfinder-6321f-default-rtdb.firebaseio.com",
-  projectId: "parkingfinder-6321f",
-  storageBucket: "parkingfinder-6321f.appspot.com",
+  databaseURL: "https://Yfiles-6321f-default-rtdb.firebaseio.com",
+  projectId: "Yfiles-6321f",
+  storageBucket: "Yfiles-6321f.appspot.com",
   messagingSenderId: "955958422328",
   appId: "1:955958422328:web:10a29d2f94058cc7e2afcd"
 })
